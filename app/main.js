9dd829c7c219b0feb03bd9d3fe07a1891a2194d3
GisellaAballay <gisellaaballaybonilla@gmail.com>
2024-06-24 15:23:19 +0200

    render workList

--- app/main.js
+++ app/main.js
@@ -1,9 +1,10 @@
+
 const socialList = document.getElementById("social-list");
+const worksArticle = document.getElementById("works-list");
 
 
 
 const renderSocialLinks = () => {
-
   const socialIcons = [
     {
       icon: "<i class='bx bx-envelope'></i>", 
@@ -31,5 +32,38 @@ const renderSocialLinks = () => {
   });
 }
 
+
+const renderWorks = () => {
+  const worksList = [
+    {
+      name:"Test",
+      yearStart:"2000",
+      yearEnd:"0",
+      role:"desarrollador",
+      description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa eveniet quo ab excepturi possimus animi consequatur molestiae, ea reiciendis?"
+    },
+    {
+      name:"Test",
+      yearStart:"2000",
+      yearEnd:"0",
+      role:"desarrollador",
+      description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa eveniet quo ab excepturi possimus animi consequatur molestiae, ea reiciendis?"
+    }
+  ]
+
+  worksList.forEach((work) => {
+    worksArticle.innerHTML += `
+    <div>
+      <div>
+        <h3><a href="">${work.name}</a></h3>
+        <h4>${work.yearStart} - ${work.yearEnd != 0 ? work.yearEnd : "Actualidad"}</h4>
+      </div>
+      <h4>${work.role}</h4>
+      <p>${work.description}</p>
+    </div>`
+  });
+}
+
 renderSocialLinks()
+renderWorks()
 
